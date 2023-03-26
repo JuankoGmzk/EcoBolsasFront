071d6a0fe09fbb80364489d46c31e155fefd20c9
Juan Pablo <jbernal@intelecto.co>
2023-03-26 00:23:09 -0500

    Se agregan importanciones necesarias para el frontend sobre angular material y ngx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,14 +9,25 @@ import { SigupComponent } from './components/sigup/sigup.component';
 import { SigninComponent } from './components/signin/signin.component';
 import { TaskComponent } from './components/task/task.component';
 import { PrivateTaskComponent } from './components/private-task/private-task.component';
-
 import { AuthGuard } from './auth.guard';
 import { TokenInterceptorService } from './services/token-interceptor.service';
 import { CreateTaskComponent } from './components/create-task/create-task.component';
-
 import {  SocketWebService} from "./services/socket-web.service";
 import { CotizadorComponent } from './components/cotizador/cotizador.component';
 import { MaterialesComponent } from './components/materiales/materiales.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import {MatInputModule} from '@angular/material/input';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import {MatCardModule} from '@angular/material/card';
+import {MatSelectModule} from '@angular/material/select';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatTableModule} from '@angular/material/table';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatSliderModule} from '@angular/material/slider';
+import {MatExpansionModule} from '@angular/material/expansion';
+import { ModalModule } from 'ngx-bootstrap/modal';
 
 
 
@@ -36,7 +47,20 @@ import { MaterialesComponent } from './components/materiales/materiales.componen
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatSlideToggleModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatCardModule,
+    MatSelectModule,
+    MatToolbarModule,
+    MatTableModule,
+    MatDialogModule,
+    MatSliderModule,
+    MatExpansionModule,
+    ModalModule.forRoot()
   ],
   providers: [
     AuthGuard,
