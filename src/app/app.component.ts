9a85db0313e4aefca0622f45edc9c2abfcb44249
Juan Pablo <jbernal@intelecto.co>
2023-04-04 19:33:29 -0500

    Ajuste para intentar mejorar el toolbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { AuthService } from './services/auth.service';
 import {  SocketWebService} from "./services/socket-web.service";
+import { MatSidenav } from '@angular/material/sidenav';
 
-
+ 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  @ViewChild('sidenav') sidenav!: MatSidenav;
+
+  toggleSidenav() {
+    this.sidenav.toggle();
+  }
 
   constructor(public authService: AuthService,private socketService: SocketWebService) {}
 
