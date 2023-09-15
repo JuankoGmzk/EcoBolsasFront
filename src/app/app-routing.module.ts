87aa2e645357dda16bf4bfa35883a5c67b38942b
JuankoGmzk <juanpis0304@hotmail.com>
2023-09-14 19:09:31 -0500

    se crean las rutas de los nuevos componentes creados.
    Cotizaciones.
    Mis ots
    usuarios
    mi perfil
    ot activas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,17 @@ import { RouterModule, Routes } from '@angular/router';
 //Components
 import {TaskComponent} from './components/task/task.component';
 import {PrivateTaskComponent} from './components/private-task/private-task.component';
-import {SigupComponent} from './components/crearUsuario/sigup.component';
+import {SigupComponent} from './components/sigup/sigup.component';
 import {SigninComponent} from './components/signin/signin.component';
 import { CreateTaskComponent } from "./components/create-task/create-task.component";
 import { CotizadorComponent } from "./components/cotizador/cotizador.component";
 import { MaterialesComponent } from "./components/materiales/materiales.component";
 import { LobbyComponent } from "./components/lobby/lobby.component"
+import { MisCotizacionesComponent } from "./components/mis-cotizaciones/mis-cotizaciones.component";
+import { MisOTComponent } from './components/mis-ot/mis-ot.component';
+import { UsersComponent } from './components/users/users.component';
+import { MiPerfilComponent } from './components/mi-perfil/mi-perfil.component';
+import { OTActivasComponent } from './components/otactivas/otactivas.component'
 import { AuthGuard } from './auth.guard';
 
 const routes: Routes = [
@@ -19,9 +24,14 @@ const routes: Routes = [
   { path : 'signup', component : SigupComponent},
   { path : 'signin', component : SigninComponent},
   { path : 'createTask', component: CreateTaskComponent,canActivate: [AuthGuard]},
-  { path : 'cotizador', component: CotizadorComponent},
+  { path : 'cotizador/:id', component: CotizadorComponent},
   { path : 'materiales', component:MaterialesComponent},
-  { path : 'lobby', component:LobbyComponent}
+  { path : 'lobby', component:LobbyComponent},
+  { path : 'misCotizaciones', component:MisCotizacionesComponent},
+  { path : 'misOT', component:MisOTComponent},
+  { path : 'users', component:UsersComponent},
+  { path : 'miPerfil', component:MiPerfilComponent},
+  { path : 'oTActivas', component:OTActivasComponent}
 ];
 
 @NgModule({
