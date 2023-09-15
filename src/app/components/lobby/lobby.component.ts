8fc4c0d8eb6309cdbbc408535a0d1ec4f4cbc58d
JuankoGmzk <juanpis0304@hotmail.com>
2023-09-15 14:54:17 -0500

    Se agregan validaciones para mostrar opciones dependiendo del rol

--- src/app/components/lobby/lobby.component.ts
+++ src/app/components/lobby/lobby.component.ts
@@ -9,11 +9,25 @@ export class LobbyComponent {
   cargo: string | null = localStorage.getItem('cargo');
   nombre: string | null= localStorage.getItem('primerNombre')+' '+localStorage.getItem('primerApellido');
   genero: string | null = localStorage.getItem('genero');
+  rol : string | null  = localStorage.getItem('rol');
+  strRol = this.rol == null ? '' : this.rol;
+  pathImageGener = this.genero === 'Masculino' ? 'avatarMasculino' : 'avatarFemenino';
+  //'Administrador', 'Colaborador', 'JefeVentas', 'JefeProduccion', 'JefeEstampado'
+  permisosCotizar = ['Administrador', 'JefeVentas'];
+  permisosCotizacionesRealizadas = ['Administrador', 'JefeVentas'];
+  permisosHistoricoDeMisOT= ['Administrador', 'JefeVentas'];
+  permisosCambiarPrecios = ['Administrador'];
+  permisosUsuarios =['Administrador']; 
+  permisosCrearUsuario =['Administrador'];
+
+  blpermisosCotizar = this.permisosCotizar.includes(this.strRol)
+  blpermisosCotizacionesRealizadas = this.permisosCotizacionesRealizadas.includes(this.strRol);
+  blpermisosHistoricoDeMisOT= this.permisosHistoricoDeMisOT.includes(this.strRol)
+  blpermisosCambiarPrecios = this.permisosCambiarPrecios.includes(this.strRol)
+  blpermisosUsuarios = this.permisosUsuarios.includes(this.strRol)
+  blpermisosCrearUsuario = this.permisosCrearUsuario.includes(this.strRol)
+
 
  
-  pathImageGener = this.genero === 'Masculino' ? 'avatarMasculino' : 'avatarFemenino';
-
-
-
   mostrarSubopciones = false;
 }
