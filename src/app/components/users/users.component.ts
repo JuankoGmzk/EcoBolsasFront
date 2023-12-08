0fa1446454f81529c306df7ad82bb654f5a0bb0d
juankogmzk <juanpis0304@hotmail.com>
2023-12-08 14:13:47 -0500

    poco ajuste pero se puede editar el usuario

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import {AuthService} from '../../services/auth.service';
-
+import { MdbModalRef, MdbModalService } from 'mdb-angular-ui-kit/modal';
 
 @Component({
   selector: 'app-users',
@@ -8,13 +8,13 @@ import {AuthService} from '../../services/auth.service';
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent {
+
   usuarios = [{primerNombre:'Juan',cedula:'1000158029',cargo:'ING'}];
 
   columnas: string[] = ['primerNombre', 'cedula','cargo','Acciones'];
   
   constructor( private authService:AuthService) {
     this.getAllUsers();
-   
   }
 
   async getAllUsers(){
@@ -24,6 +24,14 @@ export class UsersComponent {
 
   VerUsuario(usuario:object){
 
+  }
+
+  EditarUsuario(usuario:object){
+    console.log((usuario as any)._id)
+  }
+
+  openModal() {
+   // this.modalRef = this.modalService.open(ModalComponent)
   }
 
 }
